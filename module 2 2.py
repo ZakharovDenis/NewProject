c6b7ce1f74a9316ed3a41a1820b40a2dfe200577
ZakharovDenis <z.d.a.52rus@mail.ru>
2024-07-25 19:42:51 +0300

    Add files via upload
    Исправлено.

--- module 2 2.py
+++ module 2 2.py
@@ -1,6 +1,6 @@
-first = 123
-second = 456
-third = 789
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
 if first==second==third:
     print(3)
 elif first==second!=third or first==third!=second or second==third!=first:
@@ -8,9 +8,9 @@ elif first==second!=third or first==third!=second or second==third!=first:
 else:
     print(0)
 
-first = 42
-second = 69
-third = 42
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
 if first==second==third:
     print(3)
 elif first==second==third or first==third!=second or second==third!=first:
